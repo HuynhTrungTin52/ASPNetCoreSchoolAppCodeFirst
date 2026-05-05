5e9664dab3dd7baf72fca610f7a15953fe891506
HuynhTrungTin52 <524k0033@student.tdtu.edu.vn>
2026-05-06 02:00:52 +0700

    Organized projects into folders

---
+++ Exercise2_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolAppCoreMVC_CF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
